9a0141aff3e44022eba971810edb3b1e444fa772
Leonardo <leonardotreze@gmail.com>
2016-06-14 02:33:41 -0300

    initial

---
+++ public/core.js
@@ -0,0 +1,59 @@
+// public/core.js
+var scotchTodo = angular.module('scotchTodo', []);
+
+function c(title, text) {
+    console.log(title);
+    console.log(text);
+    console.log('');
+}
+
+function mainController($scope, $http) {
+    $scope.formData = {};
+
+    // when landing on the page, get all todos and show them
+    $http.get('/api/todos')
+        .success(function(data) {
+            $scope.todos = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    $scope.searchMovie = function() {
+        $http.post('/api/search', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {}; // clear the form so our user is ready to enter another
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    // when submitting the add form, send the text to the node API
+    $scope.createMovie = function() {
+        $http.post('/api/todos', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {}; // clear the form so our user is ready to enter another
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    // delete a todo after checking it
+    $scope.deleteMovie = function(id) {
+        $http.delete('/api/todos/' + id)
+            .success(function(data) {
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+}
---
+++ server.js
@@ -0,0 +1,108 @@
+// server.js
+    // set up ========================
+    var express  = require('express');
+    var app      = express();                               // create our app w/ express
+    var mongoose = require('mongoose');                     // mongoose for mongodb
+    var morgan = require('morgan');             // log requests to the console (express4)
+    var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+    var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+    console.log('a');
+    // configuration =================
+    mongoose.connect('mongodb://localhost');
+    console.log('b');
+    app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+    app.use(morgan('dev'));                                         // log every request to the console
+    app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+    app.use(bodyParser.json());                                     // parse application/json
+    app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+    app.use(methodOverride());
+    console.log('cc');
+    // define model =================
+    var Movie = mongoose.model('Movie', {
+        name : String, plot : String
+    });
+    console.log('dd');
+    // routes ======================================================================
+    // api ---------------------------------------------------------------------
+    // get all todos
+    app.get('/api/todos', function(req, res) {
+        // use mongoose to get all todos in the database
+        Movie.find(function(err, todos) {
+
+            // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+            if (err)
+                res.send(err)
+
+            res.json(todos); // return all todos in JSON format
+        });
+    });
+
+    //search
+    app.post('/api/search', function(req, res) {
+        var term = req.body.searchterm;
+        var user = req.body.userid;
+        console.log('server.js');
+        console.log('term: ' + term);
+        console.log('req.body');
+        console.log(req.body);
+
+        if (term == '')
+            return;
+
+        Movie.find(
+            { name: new RegExp(term, "i")},
+            function(err, movies) {
+            
+            if (err)
+                res.send(err)
+            
+            console.log(movies);
+            res.json(movies);
+
+        });
+    });
+
+    // create todo and send back all todos after creation
+    app.post('/api/todos', function(req, res) {
+        // create a todo, information comes from AJAX request from Angular
+        Movie.create({
+            name : req.body.name,
+            done : false
+        }, function(err, todo) {
+            if (err)
+                res.send(err);
+
+            // get and return all the todos after you create another
+            Movie.find(function(err, todos) {
+                if (err)
+                    res.send(err)
+                res.json(todos);
+            });
+        });
+    });
+
+    // delete a todo
+    app.delete('/api/todos/:todo_id', function(req, res) {
+        Movie.remove({
+            _id : req.params.todo_id
+        }, function(err, todo) {
+            if (err)
+                res.send(err);
+
+            // get and return all the todos after you create another
+            Movie.find(function(err, todos) {
+                if (err)
+                    res.send(err)
+                res.json(todos);
+            });
+        });
+    });
+
+    // application -------------------------------------------------------------
+    app.get('*', function(req, res) {
+        res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    });
+
+    // listen (start app with node server.js) ======================================
+    app.listen(8080);
+    console.log("WL listening on port 8080");
