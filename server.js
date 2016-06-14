fac9ecb42f30b4823b24d0612cde392392cd4c59
Leonardo <leonardotreze@gmail.com>
2016-06-14 02:37:32 -0300

    clean

--- server.js
+++ server.js
@@ -6,22 +6,18 @@
     var morgan = require('morgan');             // log requests to the console (express4)
     var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
     var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
-    console.log('a');
     // configuration =================
     mongoose.connect('mongodb://localhost');
-    console.log('b');
     app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
     app.use(morgan('dev'));                                         // log every request to the console
     app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
     app.use(bodyParser.json());                                     // parse application/json
     app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
     app.use(methodOverride());
-    console.log('cc');
     // define model =================
     var Movie = mongoose.model('Movie', {
         name : String, plot : String
     });
-    console.log('dd');
     // routes ======================================================================
     // api ---------------------------------------------------------------------
     // get all todos
