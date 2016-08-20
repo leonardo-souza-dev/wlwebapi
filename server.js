173e8f29f498f3ea6d9111b9982992df593dbcac
Leonardo <leonardotreze@gmail.com>
2016-08-20 15:15:18 -0300

    preparing for deploy

--- server.js
+++ server.js
@@ -3,10 +3,10 @@
     var express  = require('express');
     var app      = express();                               // create our app w/ express
     var mongoose = require('mongoose');                     // mongoose for mongodb
-	var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+	//var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
     var morgan = require('morgan');             // log requests to the console (express4)
     var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
-    var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+    //var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
     var url = require('url');
     // configuration =================
     mongoose.connect('mongodb://localhost');
@@ -15,7 +15,7 @@
     app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
     app.use(bodyParser.json());                                     // parse application/json
     app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-    app.use(methodOverride());
+    //app.use(methodOverride());
 	app.set('superSecret', 'ilovescotchyscotch'); // secret variable
 	
 	// define model =================
